2a41aa79ebf040e54d73ad9d22c59002c4470a8b
Niko Raes <Niko.raes@gmail.com>
2020-09-26 11:19:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,13 +11,29 @@ input.onButtonPressed(Button.A, function () {
         basic.showString("" + (jongenmeisje[index]))
     } else if (ronde == 2) {
         if (index == 0) {
-            index = meisjes.length - 1
+            index = kusschopknuffelsla.length - 1
         } else {
             index = index - 1
         }
         basic.showString("" + (kusschopknuffelsla[index]))
+    } else if (ronde == 3) {
+        if (keuzes[0] == "J") {
+            if (index == 0) {
+                index = jongens.length - 1
+            } else {
+                index = index - 1
+            }
+            basic.showNumber(index + 1)
+        } else {
+            if (index == 0) {
+                index = meisjes.length - 1
+            } else {
+                index = index - 1
+            }
+            basic.showNumber(index + 1)
+        }
     } else {
-    	
+        control.reset()
     }
 })
 input.onButtonPressed(Button.AB, function () {
@@ -29,16 +45,22 @@ input.onButtonPressed(Button.AB, function () {
         keuzes[0] = jongenmeisje[index]
         index = 0
         basic.showString("" + (kusschopknuffelsla[index]))
-    } else {
+    } else if (ronde == 2) {
         ronde = 3
         keuzes[1] = kusschopknuffelsla[index]
         index = 0
+        basic.showNumber(index + 1)
+    } else if (ronde == 3) {
+        ronde = 4
         if (keuzes[0] == "J") {
+            basic.showString("" + (keuzes[1]))
             basic.showString("" + (jongens[index]))
-            index = 0
         } else {
-            index = index + 1
+            basic.showString("" + (keuzes[1]))
+            basic.showString("" + (meisjes[index]))
         }
+    } else {
+        control.reset()
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -52,13 +74,31 @@ input.onButtonPressed(Button.B, function () {
             index = 0
         }
         basic.showString("" + (jongenmeisje[index]))
-    } else {
+    } else if (ronde == 2) {
         if (index == kusschopknuffelsla.length - 1) {
             index = 0
         } else {
             index = index + 1
         }
         basic.showString("" + (kusschopknuffelsla[index]))
+    } else if (ronde == 3) {
+        if (keuzes[0] == "J") {
+            if (index == jongens.length - 1) {
+                index = 0
+            } else {
+                index = index + 1
+            }
+            basic.showNumber(index + 1)
+        } else {
+            if (index == 0) {
+                index = meisjes.length - 1
+            } else {
+                index = index - 1
+            }
+            basic.showNumber(index + 1)
+        }
+    } else {
+        control.reset()
     }
 })
 let jongens: string[] = []
@@ -79,6 +119,6 @@ keuzes = []
 ronde = 0
 index = 0
 jongenmeisje = ["J", "M"]
-kusschopknuffelsla = ["KUS", "SLA", "KNUFFEL", "SCHOP"]
-meisjes = ["RENEE", "MEREL", "MIA", "TENZIN", "MARILOU", "FEBE", "JANNE", "ANNABEL"]
-jongens = ["JENS", "JEROM", "TIAGO", "MIROM", "MOHAMED", "BRIEK", "KIES", "FELIX"]
+kusschopknuffelsla = ["KUS", "SLA", "KNUF", "SCHOP"]
+meisjes = ["RENEE", "MEREL", "MIA", "TENZ", "MARI", "FEBE", "JANNE", "ANNA"]
+jongens = ["JENS", "JERO", "TIAG", "MIRO", "MO", "BRIE", "KIES", "FELI"]
